c1254db6769008bf0d1937ccc27c42c2012c8c50
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-02-14 15:31:13 -0700

    Add change quality functionality

--- scripts.js
+++ scripts.js
@@ -2,19 +2,19 @@
 $('.saveBtn').on('click', function() {
   var $titleInput = $('.userTitle').val();
   var $bodyInput = $('.userBody').val();
-  console.log($titleInput);
+  var $uniqueId = event.timeStamp;
   $('.ideaCards').prepend(
     `<div class="ideaCard">
-        <div class="cardTop">
-          <h2 class="ideaTitle">${$titleInput}</h2>
+        <div class="cardTop" id="${$uniqueId}">
+          <h2 class="ideaTitle" contenteditable="true">${$titleInput}</h2>
           <input class="deleteBtn icon" type="image" src="FEE-ideabox-icon-assets/delete.svg">
         </div>
-        <p class="ideaBody">${$bodyInput}</p>
+        <p class="ideaBody" contenteditable="true">${$bodyInput}</p>
         <div class="cardBottom">
           <input class="upvote vote icon" type="image" src="FEE-ideabox-icon-assets/upvote.svg">
           <input class="downvote vote icon" type="image" src="FEE-ideabox-icon-assets/downvote.svg">
           <p class="quality">quality:
-            <span class="quality">swill</span>
+            <span class="quality qualityLevel">swill</span>
           </p>
         </div>
       </div>`)
@@ -29,3 +29,29 @@ function clearFields() {
   $('.userTitle').val("")
   $('.userBody').val("");
 }
+
+$('.ideaCards').on('click', '.upvote', function() {
+  var $quality = $('.qualityLevel').html();
+  if ($quality == "swill") {
+    $('.qualityLevel').text("plausible")
+  }
+  else if ($quality === "plausible") {
+    $('.qualityLevel').text("genius")
+  }
+  else {
+    $('.qualityLevel').text("genius")
+  }
+})
+
+$('.ideaCards').on('click', '.downvote', function() {
+  var $quality = $('.qualityLevel').html();
+  if ($quality == "genius") {
+    $('.qualityLevel').text("plausible")
+  }
+  else if ($quality === "plausible") {
+    $('.qualityLevel').text("swill")
+  }
+  else {
+    $('.qualityLevel').text("swill")
+  }
+})
