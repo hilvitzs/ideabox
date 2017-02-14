18cc322bf1d2c7281ff1a2928280ba163d563306
Spencer Hilvitz <spencer.hilvitz@colorado.edu>
2017-02-14 13:03:51 -0700

    Add jQuery - populate user information

---
+++ scripts.js
@@ -0,0 +1,26 @@
+$('.saveBtn').on('click', function() {
+  var $titleInput = $('.userTitle').val();
+  var $bodyInput = $('.userBody').val();
+  console.log($titleInput);
+  $('.ideaCards').prepend(
+    `<div class="ideaCard">
+        <div class="cardTop">
+          <h2 class="ideaTitle">${$titleInput}</h2>
+          <input class="delete icon" type="image" src="FEE-ideabox-icon-assets/delete.svg">
+        </div>
+        <p class="ideaBody">${$bodyInput}</p>
+        <div class="cardBottom">
+          <input class="upvote vote icon" type="image" src="FEE-ideabox-icon-assets/upvote.svg">
+          <input class="downvote vote icon" type="image" src="FEE-ideabox-icon-assets/downvote.svg">
+          <p class="quality">quality:
+            <span class="quality">swill</span>
+          </p>
+        </div>
+      </div>`)
+  clearFields();
+});
+
+function clearFields() {
+  $('.userTitle').val() = "";
+  $('.userBody').val() = "";
+}
