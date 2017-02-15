dac5a4e9cc1b8b36897de43dd22663b8c2c0c20a
Leta Keane <letakeane+turing@gmail.com>
2017-02-14 18:07:04 -0700

    Add constructor function to create objects, stringify and parse data, display unique card

--- scripts.js
+++ scripts.js
@@ -1,26 +1,66 @@
+window.onload = function() {
+  displayOnPageLoad();
+}
 
-$('.saveBtn').on('click', function() {
+function displayOnPageLoad() {
+  var keys = Object.keys(localStorage);
+  for (var i = 0; i < localStorage.length; i++) {
+    var parsedIdea = JSON.parse(localStorage[keys[i]]);
+    prependNew(parsedIdea);
+  }
+}
+
+function NewIdea(id, title, body, quality) {
+  this.id = id;
+  this.title = title;
+  this.body = body;
+  this.quality = "swill" || quality;
+}
+
+function grabValues() {
+  var $uniqueId = event.timeStamp;
   var $titleInput = $('.userTitle').val();
   var $bodyInput = $('.userBody').val();
-  var $uniqueId = event.timeStamp;
+  stringifyIdea($uniqueId, $titleInput, $bodyInput);
+}
+
+function stringifyIdea(id, title, body) {
+  var newIdea = new NewIdea(id, title, body);
+  var stringIdea = JSON.stringify(newIdea);
+  localStorage.setItem(id, stringIdea);
+  parseNewestIdea(id);
+}
+
+function parseNewestIdea(id) {
+  var singleParse = JSON.parse(localStorage.getItem(id));
+  prependNew(singleParse);
+}
+
+function prependNew(parsedIdea) {
   $('.ideaCards').prepend(
     `<div class="ideaCard">
-        <div class="cardTop" id="${$uniqueId}">
-          <h2 class="ideaTitle" contenteditable="true">${$titleInput}</h2>
+        <div class="cardTop" id="${parsedIdea.id}">
+          <h2 class="ideaTitle" contenteditable="true">${parsedIdea.title}</h2>
           <div class="deleteBtn icon">
           </div>
         </div>
-        <p class="ideaBody" contenteditable="true">${$bodyInput}</p>
+        <p class="ideaBody" contenteditable="true">${parsedIdea.body}</p>
         <div class="cardBottom">
           <div class="upvote vote icon">
           </div>
           <div class="downvote vote icon">
           </div>
           <p class="quality">quality:
-            <span class="quality qualityLevel">swill</span>
+            <span class="quality qualityLevel">${parsedIdea.quality}</span>
           </p>
         </div>
       </div>`)
+}
+
+$('.saveBtn').on('click', function() {
+  grabValues();
+  // console.log(newIdea);
+
   clearFields();
 });
 
